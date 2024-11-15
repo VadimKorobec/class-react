267be7e436a284df5ff9945b333f0324dcd2ecfa
Vadim <_s_e_t__@ukr.net>
2024-11-15 13:46:06 +0200

    add some code

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -8,6 +8,19 @@ const DUMMY_USERS = [
   { id: 'u2', name: 'Manuel' },
   { id: 'u3', name: 'Julie' },
 ];
+
+class Users extends Component{
+  render() {
+    return (
+      <div className={classes.users}>
+        <button onClick={toggleUsersHandler}>
+          {showUsers ? "Hide" : "Show"} Users
+        </button>
+        {showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
